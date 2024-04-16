baf03ec6c4c694502e1475fb53db574764ddd4fd
khamidullo <khkhamidullo@gmail.com>
2024-04-16 20:38:59 +0500

    delete not nessanery folder

--- 8-dars/test.js
+++
@@ -1,8 +0,0 @@
-
-func("salom dunyo nimadur")("a", "A")
-func("salom dunyo nimadur")("a")("A")
-
-// TODO: Natija shunday bo'lmog'ligi lozim
-func("sAlom dunyo nimAdur")("a", "A")
-
-// console.log("a".toUpperCase())
