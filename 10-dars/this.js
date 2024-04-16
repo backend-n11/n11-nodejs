e9f465197e7316f6ccb58d60727105af809b9434
khamidullo <khkhamidullo@gmail.com>
2024-04-16 20:40:31 +0500

    delete not nessasery folder

--- 10-dars/this.js
+++
@@ -1,8 +0,0 @@
-let str = ''
-
-for (let i = 65; i <= 220; i++) {
-  str += String.fromCodePoint(i)
-}
-console.log(str)
-
-console.log(String.fromCharCode(66))
--- 8-dars/fac.js
+++
@@ -1,7 +0,0 @@
-// function calc(bola, ota) {
-//   bola *= 2;
-//   console.log(ota - bola);
-// }
-//
-// calc(23, 81);
-//
