6ebbe3bc014eba0a4cb358642e7905a93abe712b
khamidullo <khkhamidullo@gmail.com>
2024-05-29 10:17:59 +0500

    46 dars error handle

---
+++ src/middlewares/httpErrorhandle.js
@@ -0,0 +1,12 @@
+import {
+  HTTP_RESPONSE_CODE,
+  APP_ERROR_MESSAGE
+} from "../constants/HttpstatusCodes.js"
+
+
+export class MyError extends Error {
+  constructor (message, statuCode) {
+    super(message)
+    this.statuCode = statuCode
+  }
+}
