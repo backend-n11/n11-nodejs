06bf9a4deb95c26f473ebf548dd85a685d13c8e0
khamidullo <khkhamidullo@gmail.com>
2024-07-03 10:12:49 +0500

    implement user route with mongodb

--- nest-two/src/app.module.ts
+++ nest-two/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from "@nestjs/mongoose";
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/nest'), UserModule],
   controllers: [],
   providers: [],
 })
